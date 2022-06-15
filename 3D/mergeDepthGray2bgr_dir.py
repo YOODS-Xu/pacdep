efb651f5ce0de9c417fc303f2148a64ab2037073
DESKTOP-MCEQBTI\xu168 <xu168@yoods.co.jp>
2022-06-15 17:16:12 +0900

    一時バックアップ

---
+++ 3D/mergeDepthGray2bgr_dir.py
@@ -0,0 +1,104 @@
+#Merge depth and gray scale to bgr bmp file.
+
+import os
+from pathlib import Path
+#from telnetlib import GA
+import numpy as np
+import cv2
+
+DEPTH_FILE_PREFIX = "depth"
+GRAY_FILE_PREFIX = "gray"
+MERGED_FILE_PREFIX = "merged"
+
+def check_path(input_path):
+    """ディレクトリとファイルの存在チェック
+    Args:
+        input_path(string): ディレクトリまたはファイル
+
+    Raises:
+        OSError: 存在しない場合に生じます
+    """
+
+    path_name = Path(input_path)
+
+    if not path_name.exists():
+        raise OSError(2, "No such directory or file", str(path_name))
+dtype=np.uint8
+
+
+def merge(bArray, gArray, rArray):
+
+    high, w = bArray.shape
+    
+    bgrArray = np.concatenate(\
+        [bArray.reshape(high, w, 1), gArray.reshape(high, w, 1), rArray.reshape(high, w, 1)], \
+            axis=2)
+    
+    return bgrArray
+
+
+
+def main(srcDir, dstDir, depthBGRtype):
+
+    #check 変換前画像ディレクトリ
+    check_path(srcDir)
+
+    #変換後ディレクトリ作成、既に存在した場合、エラー上げて終了
+    Path(dstDir).mkdir(parents=True, exist_ok=False)
+
+    gray_fname_list = [gray_fname for gray_fname in os.listdir(srcDir) if gray_fname.startswith(GRAY_FILE_PREFIX)]
+
+    for gray_fname in gray_fname_list:
+       
+        depth_fname = DEPTH_FILE_PREFIX + gray_fname[len(GRAY_FILE_PREFIX):]
+
+        #ディレクトリ名を追加してファイルパス
+        depth_path = str(Path(srcDir).joinpath(depth_fname))
+        gray_path = str(Path(srcDir).joinpath(gray_fname))
+        
+        check_path(depth_path)
+
+        #depth bmpファイルを読み取り
+        depth_img = cv2.imread(depth_path)
+        #depth bmpはbgr同じのgray scale
+        depth_array = cv2.split(depth_img)
+        depth_r = depth_array[0]
+
+        #gray bmpファイルを読み取り
+        gray_img = cv2.imread(gray_path)
+        #gray bmpはbgr同じのgray scale
+        gray_array =  cv2.split(gray_img)
+        gray_r = gray_array[0]
+       
+        if depthBGRtype == "b":
+            #b:depth g:gray r:gray
+            bgr_img = merge(depth_r, gray_r, gray_r)
+        elif depthBGRtype == "g":
+            #b:gray g:depth r:gray
+            rgb_img = merge(gray_r, depth_r, gray_r)
+        else:
+            #b:gray g:gray r:depth
+            rgb_img = merge(gray_r, gray_r, depth_r)
+
+        #マージ後ファイルのパスを作成:bmpのまま
+        dst_path = str(Path(dstDir).joinpath(MERGED_FILE_PREFIX + gray_fname[len(GRAY_FILE_PREFIX):]))
+          
+        cv2.imwrite(dst_path, bgr_img)
+
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(
+        description='Merge depth and gray scale to bgr bmp file.',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--srcDir', help='directory of source depth & gray bmp file', default='Depth+Gray', type=str)   
+    parser.add_argument('--dstDir', help='directory of destination bgr bmp file', default='MergedBGR', type=str)
+    parser.add_argument('--depthBGRtype', help='use depth as b, g or r', default='b', type=str)
+    
+    args = parser.parse_args()
+    
+    try:
+        main(args.srcDir, args.dstDir, args.depthBGRtype)
+    except Exception as e:
+        print(e)
